769524d78bcb1575beeeed4144822ca21caf5574
SilvanaAlberdi <silvanaalberdi@ni.eus>
2023-01-31 12:53:45 +0100

    Programming

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private float forwardInput;
     private Rigidbody _rigidbody;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupForce = 15f;
     
     private void Start()
     {
@@ -19,5 +21,31 @@ public class PlayerController : MonoBehaviour
     {
         forwardInput = Input.GetAxis("Vertical");
         _rigidbody.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            StartCoroutine(PowerupCountDown());
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position).normalized;
+            enemyRigidbody.AddForce(awayFromPlayer * powerupForce, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerupCountDown()
+    {
+        yield return new WaitForSeconds(6);
+        hasPowerup = false;
     }
 }
